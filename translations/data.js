2282bc5d3705917f5a658a4b260ae0e80274befb
Vasyl Shpak <vasyl.shpak@grandparade.co.uk>
2022-12-13 18:05:48 +0100

    test(dcx-exercise): add test scenarios for task, update readme

---
+++ translations/data.js
@@ -0,0 +1,10 @@
+const text = {
+    Salesforce: 'DCX is a Salesforce Consulting Partner. Our specialist Salesforce practice, covering Sales Cloud, Marketing Cloud, and Experience Cloud, helps organisations of all sizes collect and understand customer data, and build more personalised, relevant, and seamless customer journeys - all with the power of the world\'s #1 CRM.',
+    Acquia: 'DCX is an Acquia Silver Partner. Whether you\'re looking to re-platform your website onto the Acquia CMS, make full use of Acquia Site Studio, Content Hub, DAM, Customer Data Platform, or Campaign Studio, our expert team will guide you along the way.',
+    Acoustic: 'Acoustic Marketing Cloud is a simple yet powerful platform that helps you develop a deeper pool of knowledge on your customer’s behaviour, and create campaigns that engage and drive revenue. Our expert team will help you communicate with customers at scale, on a personalised level, to dramatically grow your business.',
+    Adobe: 'Adobe Campaign allows your organisation to use rich customer data to create, co-ordinate and deliver dynamic campaigns that customers actually want — through email, mobile, off-line channels and more. DCX agency ',
+    Bynder: 'Bynder’s digital asset management platform (DAM) enables your team to centralise and manage your digital assets in one place, collaborate creatively in the cloud and get content to market faster.',
+    Sitecore: 'DCX has been working with the Sitecore platform for over 10 years. We have designed, built, and optimised numerous websites on Sitecore, integrating with lots of of different systems, and have even won Best Use of Ominchannel Automation in the annual Sitecore Experience Awards.'
+}
+
+module.exports = text;
