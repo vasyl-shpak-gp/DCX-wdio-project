f6c42cd17fe991d77b81dfc6a78d61daaf2600e2
Vasyl Shpak <vasyl.shpak@grandparade.co.uk>
2022-12-13 13:27:51 +0100

    test(dcx-exercise): init project setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DCX-wdio-project
+
+node 14 run test - npx wdio run wdio.conf.js
---
+++ test/specs/example.e2e.js
@@ -0,0 +1,13 @@
+const LoginPage = require('../pageobjects/login.page')
+const SecurePage = require('../pageobjects/secure.page')
+
+describe('List of capabilities', () => {
+    it('should login with valid credentials', async () => {
+        //Open the main page
+        //On the header locate the capabilities dropdown
+        //Ensure that the capabilities dropdown list contains [Products & Platforms, Technology Expertise] Select Technology Expertise and ensure that the page contains a list of technologies
+        //Each of the technology blocks should contain: Name, Image, Description]
+    })
+})
+
+
